39856566c9ea1af16a6b5fc72ebdaa9136859a5b
Loumarven Payot <lou.payot@gmail.com>
2020-05-17 17:41:23 +0800

    Initial implementation for LinkedList

---
+++ lib/linked_list.rb
@@ -0,0 +1,33 @@
+class LinkedList
+  attr_reader :head, :tail, :size
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @size = 0
+  end
+
+  def append(node)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      @tail.next_node = node
+      @tail = node
+    end
+
+    @size += 1
+  end
+
+  def prepend(node)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      node.next_node = @head
+      @head = node
+    end
+
+    @size += 1
+  end
+end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,1 +1,36 @@
 require 'linked_list'
+
+RSpec.describe LinkedList, "#append" do
+  it "adds a new node to the end of the list" do
+    linked_list = LinkedList.new
+    node = Node.new(50)
+      
+    linked_list.append(node)
+
+    expect(linked_list.tail).to eq node
+  end
+end
+
+RSpec.describe LinkedList, "#prepend" do
+  it "adds a new node to the start of the list" do
+    linked_list = LinkedList.new
+    node = Node.new(50)
+
+    linked_list.prepend(node)
+
+    expect(linked_list.head).to eq node
+  end
+end
+
+RSpec.describe LinkedList, "#size" do
+  it "returns the total number of nodes in the list" do
+    linked_list = LinkedList.new
+    node = Node.new(50)
+    node2 = Node.new(60)
+
+    linked_list.append(node)
+    linked_list.append(node2)
+
+    expect(linked_list.size).to eq 2
+  end
+end
