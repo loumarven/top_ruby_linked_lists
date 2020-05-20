ff9dc7543be8f96339b2fd3baa476319b4be8ee3
Loumarven Payot <lou.payot@gmail.com>
2020-05-20 15:45:53 +0800

    Refactor append, add at and pop methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -13,7 +13,7 @@ class LinkedList
       @tail = node
     else
       @tail.next_node = node
-      @tail = node
+      @tail = @tail.next_node
     end
 
     @size += 1
@@ -29,5 +29,40 @@ class LinkedList
     end
 
     @size += 1
+  end
+
+  def at(index)
+    return nil if index > @size - 1 || index < 0
+
+    node = @head
+    (0...index).each { |tmp| node = node.next_node }
+
+    node
+  end
+
+  def pop
+    return nil if @size == 0
+
+    old_last = @head
+    new_last = nil
+
+    if @size == 1
+      @head = nil
+      @tail = nil
+      @size -= 1
+
+      return old_last
+    end
+
+    (0...@size - 1).each do |tmp|
+      new_last = old_last
+      old_last = old_last.next_node
+    end
+
+    new_last.next_node = nil
+    @tail = new_last
+    @size -= 1
+
+    old_last
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -34,3 +34,114 @@ RSpec.describe LinkedList, "#size" do
     expect(linked_list.size).to eq 2
   end
 end
+
+RSpec.describe LinkedList, "#at" do
+  context "when the index is valid" do
+    it "returns the node at the given index" do
+      linked_list = LinkedList.new
+      node = Node.new(50)
+      node2 = Node.new(60)
+      node3 = Node.new(70)
+      node4 = Node.new(80)
+
+      linked_list.append(node)
+      linked_list.append(node2)
+      linked_list.append(node3)
+      linked_list.append(node4)
+
+      expect(linked_list.at(2)).to eq node3
+    end
+  end
+
+  context "when the index is greater than #size" do
+    it "returns nil" do
+      linked_list = LinkedList.new
+      node = Node.new(50)
+      node2 = Node.new(60)
+      node3 = Node.new(70)
+      node4 = Node.new(80)
+
+      linked_list.append(node)
+      linked_list.append(node2)
+      linked_list.append(node3)
+      linked_list.append(node4)
+
+      expect(linked_list.at(4)).to be_nil
+    end
+  end
+
+  context "when the index negative" do
+    it "returns nil" do
+      linked_list = LinkedList.new
+      node = Node.new(50)
+      node2 = Node.new(60)
+      node3 = Node.new(70)
+      node4 = Node.new(80)
+
+      linked_list.append(node)
+      linked_list.append(node2)
+      linked_list.append(node3)
+      linked_list.append(node4)
+
+      expect(linked_list.at(-2)).to be_nil
+    end
+  end
+end
+
+RSpec.describe LinkedList, "#pop" do
+  context "when there are many nodes" do
+    it "removes the last element from the list" do
+      linked_list = LinkedList.new
+      node = Node.new(50)
+      node2 = Node.new(60)
+      node3 = Node.new(70)
+      node4 = Node.new(80)
+
+      linked_list.append(node)
+      linked_list.append(node2)
+      linked_list.append(node3)
+      linked_list.append(node4)
+      
+      expect(linked_list.size).to eq 4
+
+      popped = linked_list.pop
+
+      expect(popped).to eq node4
+      expect(linked_list.size).to eq 3
+      expect(linked_list.tail).to eq node3
+    end
+  end
+
+  context "when there is 1 node" do
+    it "empties the list" do
+      linked_list = LinkedList.new
+      node = Node.new(50)
+
+      linked_list.append(node)
+
+      expect(linked_list.size).to eq 1
+
+      popped = linked_list.pop
+
+      expect(popped).to eq node
+      expect(linked_list.size).to eq 0
+      expect(linked_list.head).to be_nil
+      expect(linked_list.tail).to be_nil
+    end
+  end
+
+  context "when the list is empty" do
+    it "pops nothing" do
+      linked_list = LinkedList.new
+
+      expect(linked_list.size).to eq 0
+
+      popped = linked_list.pop
+
+      expect(popped).to be_nil
+      expect(linked_list.size).to eq 0
+      expect(linked_list.head).to be_nil
+      expect(linked_list.tail).to be_nil
+    end
+  end
+end
